9d6dca53c8f7e32e1e39e64839d1cf4dd2ac2ef3
munenebrian <njerubrianmunene@gmail.com>
2022-09-16 11:22:39 +0300

    getting the frontend working

---
+++ work-tracker/src/components/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function About() {
+  return (
+    <div className='about'>
+        <p><h1>ASSIGN WORK</h1><h3>Input work to be done and details about to be done. <br/> <h2>Make sure to state your position and category</h2> </h3></p>
+
+
+    </div>
+  )
+}
+
+export default About
